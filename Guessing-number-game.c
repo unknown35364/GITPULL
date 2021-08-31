bbbbb9610f37c00e342900e2dafb8bb747ad448c
unknown35364 <89828474+unknown35364@users.noreply.github.com>
2021-08-31 10:49:15 +0530

    Create Guessing-number-game.c

---
+++ Guessing-number-game.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+ printf("Guessing number Game\n");
+ printf("\n");
+ printf("Enter the number:");
+ scanf("%d",&i);
+ if(i==5)
+ printf("You Win");
+ else
+ printf("You Loss");
+ }
